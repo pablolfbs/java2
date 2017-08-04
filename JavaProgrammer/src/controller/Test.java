01e1310dc1df523b6a1ed3d8ab87bc81433433de
pablo <pablo@PABLO>
2017-08-03 21:47:39 -0300

    mn

--- JavaProgrammer/src/controller/Test.java
+++ JavaProgrammer/src/controller/Test.java
@@ -3,27 +3,31 @@ package controller;
 import model.E;
 
 class Test {
-	
+
 	public static void main(String[] args) {
-		
-		StringBuilder sb =  new StringBuilder();
-		sb.append("Pablo ").append("Luiz ").append("Faria ")
-		.append("Britto ").append("de ").append("Souza").reverse().toString();
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("Pablo ").append("Luiz ").append("Faria ").append("Britto ").append("de ").append("Souza").reverse()
+				.toString();
 		System.out.println(sb);
 		System.out.println(sb.indexOf("a"));
 		System.out.println(sb.lastIndexOf("a"));
-		
+
 		String str = "             Pablo  Faria          ".replace("  ", " ");
 		System.out.println(str.trim().toUpperCase());
-		
+
 		String empty = null;
 		String full = "Pablo" + empty;
 		System.out.println(full);
-		
+
 		String s = "guilherme";
-		s = s.substring(0,3);
+		s = s.substring(0, 3);
 		System.out.println(s);
+
+		E e = new E();
+		e.sortidos();
+		e.sortidos2();
+		e.jogo();
 		
-		new E().soma();
 	}
 }
--- JavaProgrammer/src/model/E.java
+++ JavaProgrammer/src/model/E.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class E {
@@ -13,6 +15,54 @@ public class E {
 		int b = in.nextInt();
 		int c = a + b;
 		System.out.println("O resultado é: " + c);
+		in.close();
 	}
-	
+
+	public void sortidos() {
+
+		ArrayList<Integer> arl = new ArrayList<Integer>();
+		Random rand = new Random();
+		for (int i = 1; i <= 50; i++) {
+			arl.add(rand.nextInt(100));
+		}
+		System.out.println(arl);
+	}
+
+	public void sortidos2() {
+
+		int[] array = new int[50];
+		Random rand = new Random();
+		for (int i = 0; i < array.length; i++) {
+			array[i] = rand.nextInt(100);
+			System.out.print(array[i] + " ");
+		}
+	}
+
+	public void jogo() {
+
+		Scanner in = new Scanner(System.in);
+		Random rand = new Random();
+		int a;
+		int count = 0;
+		int x = rand.nextInt(100);
+		System.out.println(x);
+		int i = 0;
+		System.out.println("Tente adivinhar o número!!");
+		System.out.println("Digite um número de 1 a 100");
+		a = in.nextInt();
+		if (a > x) {
+			System.out.println("Muito alto!!");
+			a = in.nextInt();
+			count++;
+		}
+		if (a < x) {
+			System.out.println("Muito baixo!!");
+			a = in.nextInt();
+			count++;
+
+		}
+		if (a == x) {
+			System.out.println("Parabéns!!!!");
+		}
+	}
 }
