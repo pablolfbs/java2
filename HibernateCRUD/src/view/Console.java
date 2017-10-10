31e38a7df69ada3ca4710bd7fe4c28913e713fcb
pablo <pablo@PABLO>
2017-10-10 15:41:31 -0300

    aaa

--- HibernateCRUD/src/view/Console.java
+++ HibernateCRUD/src/view/Console.java
@@ -14,38 +14,47 @@ import model.Venda;
 public class Console {
 
 	Scanner s = new Scanner(System.in);
+	
+	int s1, s2;
 
 	public Console() {
 		menu();
 	}
 
 	private void menu() {
-
-		System.out.println("1 - Gerenciar Cliente");
-		System.out.println("2 - Gerenciar Produto");
-		System.out.println("3 - Gerenciar Venda");
-
-		int esc1 = s.nextInt();
 		
-		System.out.println("1 - Inserir");
-		System.out.println("2 - Atualizar");
-		System.out.println("3 - Remover");
-		int esc2 = s.nextInt();
+		System.out.println("-----------------------");
+		System.out.println("|  ESCOLHA UMA OPÇÃO  |");
+		System.out.println("|1 - Gerenciar Cliente|");
+		System.out.println("|2 - Gerenciar Produto|");
+		System.out.println("|3 - Gerenciar Venda  |");
+		System.out.println("|---------------------|");
+
+		s1 = s.nextInt();
 		
-		escolha(esc1);
+		System.out.println("------------------------");
+		System.out.println("|  ESCOLHA UMA OPÇÃO  |");
+		System.out.println("|1 - Inserir          |");
+		System.out.println("|2 - Atualizar        |");
+		System.out.println("|3 - Remover          |");
+		System.out.println("|---------------------|");
+		
+		s2 = s.nextInt();
+		
+		escolha();
 }
 
-	private void escolha(int esc1) {
+	private void escolha() {
 
-		switch (esc1) {
+		switch (s1) {
 		case 1:
-			gerenciaCliente(esc1);
+			gerenciaCliente();
 			break;
 		case 2:
-			gerenciaProduto(esc1);
+			gerenciaProduto();
 			break;
 		case 3:
-			gerenciaVenda(esc1);
+			gerenciaVenda();
 			break;
 		default:
 			System.out.println("Valor inválido! Tente novamente!");
@@ -54,47 +63,57 @@ public class Console {
 
 	}
 
-	private void gerenciaCliente(int esc2) {
-		switch (esc2) {
+	private void gerenciaCliente() {
+		switch (s2) {
 		case 1:
 			insereCliente();
 			break;
 		case 2:
-			insereProduto();
+			atualizaCliente();
 			break;
 		case 3:
-			insereVenda();
+			removeCliente();
 			break;
 
 		default:
 			break;
 		}
 	}
+	
+	private void gerenciaProduto() {
+		switch (s2) {
+		case 1:
+			insereProduto();
+			break;
+		case 2:
+			atualizaProduto();
+			break;
+		case 3:
+			removeProduto();
+			break;
 
-	private void insereVenda() {
-		/*
-		Venda v = new Venda();
-		System.out.println("Insira o nome:");
-		v.setCliente(s.next());			
-		System.out.println();
-		System.out.println("Insira o cpf:");
-		v.set(s.next());
-		VendaDAO vDAO = VendaDAO.getInstancia();
-		vDAO.persist(v);
-		*/
+		default:
+			break;
+		}
 	}
+	
+	private void gerenciaVenda() {
+		switch (s2) {
+		case 1:
+			insereVenda();
+			break;
+		case 2:
+			atualizaVenda();
+			break;
+		case 3:
+			removeVenda();
+			break;
 
-	private void insereProduto() {
-		Produto p = new Produto();
-		System.out.println("Insira o nome:");
-		p.setNome(s.next());			
-		System.out.println();
-		System.out.println("Insira o preço:");
-		p.setPreco(s.nextFloat());
-		ProdutoDAO pDAO = ProdutoDAO.getInstancia();
-		pDAO.persist(p);
+		default:
+			break;
+		}
 	}
-
+	
 	private void insereCliente() {
 		Cliente c = new Cliente();
 		System.out.println("Insira o nome:");
@@ -108,13 +127,70 @@ public class Console {
 		ClienteDAO cDAO = ClienteDAO.getInstancia();
 		cDAO.persist(c);
 	}
-
-	private void gerenciaProduto(int esc2) {
-
+	
+	private void atualizaCliente() {
+		Cliente c = new Cliente();
+		System.out.println("Insira o código do cliente:");
+		c.setCod(s.nextInt());
+		System.out.println("Insira o nome:");
+		c.setNome(s.next());
+		System.out.println();
+		System.out.println("Insira o cpf:");
+		c.setCpf(s.next());
+		System.out.println();
+		System.out.println("Insira o telefone:");
+		c.setTelefone(s.next());
+		ClienteDAO cDAO = ClienteDAO.getInstancia();
+		cDAO.merge(c);
+	}
+	
+	private void removeCliente() {
+		Cliente c = new Cliente();
+		System.out.println("Qual cliente deseja remover?");
+		c.setCod(s.nextInt());
+		ClienteDAO cDAO = ClienteDAO.getInstancia();
+		cDAO.remove(Cliente.class, c.getCod());
+		
+	}
+	
+	private void insereProduto() {
+		Produto p = new Produto();
+		System.out.println("Insira o nome:");
+		p.setNome(s.next());			
+		System.out.println();
+		System.out.println("Insira o preço:");
+		p.setPreco(s.nextFloat());
+		ProdutoDAO pDAO = ProdutoDAO.getInstancia();
+		pDAO.persist(p);
+	}
+	
+	private void atualizaProduto() {
+		
+	}
+	
+	private void removeProduto() {
+		
+	}
+	
+	private void insereVenda() {
+		/*
+		Venda v = new Venda();
+		System.out.println("Insira o nome:");
+		v.setCliente(s.next());			
+		System.out.println();
+		System.out.println("Insira o cpf:");
+		v.set(s.next());
+		VendaDAO vDAO = VendaDAO.getInstancia();
+		vDAO.persist(v);
+		 */
 	}
 
-	private void gerenciaVenda(int esc2) {
-
+	private void atualizaVenda() {
+		
 	}
-
+	
+	private void removeVenda() {
+		
+	}
+	
 }
