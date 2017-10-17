0b41264ddf7166df3a50a4cab24aef82f01ea33e
Pablo <pablo.fariabritto@gmail.com>
2017-10-17 13:47:23 -0200

    Criação do Banco de Dados do Projeto de Hotelaria do Prof. Agnaldo

--- Hotelaria/src/model/Tbl.java
+++ Hotelaria/src/model/Tbl.java
@@ -2,18 +2,19 @@ package model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Tbl {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	
 	@Column
-	private String nome;
+	private String cliente;
 	
 	@Column
 	private int qtdHospedes;
@@ -25,18 +26,98 @@ public class Tbl {
 	private String checkout;
 	
 	@Column
-	private String noites;
+	private int noites;
 	
 	@Column
-	private String quarto;
+	private String tipo_uh;
 	
 	@Column
-	private float valorDiaria;
+	private String vl_diaria;
 	
 	@Column
-	private float valorPeriodo;
+	private String rs_uh;
 	
 	@Column
-	private float gastosExtras;
+	private String gastos_extras;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(String cliente) {
+		this.cliente = cliente;
+	}
+
+	public int getQtdHospedes() {
+		return qtdHospedes;
+	}
+
+	public void setQtdHospedes(int qtdHospedes) {
+		this.qtdHospedes = qtdHospedes;
+	}
+
+	public String getCheckin() {
+		return checkin;
+	}
+
+	public void setCheckin(String checkin) {
+		this.checkin = checkin;
+	}
+
+	public String getCheckout() {
+		return checkout;
+	}
+
+	public void setCheckout(String checkout) {
+		this.checkout = checkout;
+	}
+
+	public int getNoites() {
+		return noites;
+	}
+
+	public void setNoites(int noites) {
+		this.noites = noites;
+	}
+
+	public String getTipo_uh() {
+		return tipo_uh;
+	}
+
+	public void setTipo_uh(String tipo_uh) {
+		this.tipo_uh = tipo_uh;
+	}
+
+	public String getVl_diaria() {
+		return vl_diaria;
+	}
+
+	public void setVl_diaria(String vl_diaria) {
+		this.vl_diaria = vl_diaria;
+	}
+
+	public String getRs_uh() {
+		return rs_uh;
+	}
+
+	public void setRs_uh(String rs_uh) {
+		this.rs_uh = rs_uh;
+	}
+
+	public String getGastos_extras() {
+		return gastos_extras;
+	}
+
+	public void setGastos_extras(String gastos_extras) {
+		this.gastos_extras = gastos_extras;
+	}
 
 }
--- Hotelaria/src/teste/TesteDao.java
+++ Hotelaria/src/teste/TesteDao.java
@@ -1,27 +1,37 @@
 package teste;
 
-import java.util.List;
-
-import dao.GenericDao;
 import dao.TblDao;
 import model.Tbl;
-import modelo.Venda;
 
 public class TesteDao {
 
 	public static void main(String[] args) {
 
-		TblDao h = TblDao.getInstancia();
+//		TblDao h = TblDao.getInstancia();
+		
+//		Tbl tbl = new Tbl();
+//		tbl.setCliente("Pablo");
+//		tbl.setQtdHospedes(5);
+//		tbl.setCheckin("12/29/2017");
+//		tbl.setCheckout("01/02/2018");
+//		tbl.setNoites(3);
+//		tbl.setTipo_uh("Premium");
+//		tbl.setVl_diaria("R$500,00");
+//		tbl.setRs_uh("R$1.000,00");
+//		tbl.setGastos_extras("R$200,00");
+		
+//		h.persistir(tbl);
 
-		// dao.persistir(h);
-		// dao.buscaPorId(Tbl.class, 2);
-
-		List<Tbl> resultado = (List<Tbl>) h.buscaPorId(Tbl.class, 1);
-
-		for (Tbl tbl : resultado) {
-			System.out.println(
-					tbl.getCliente().getNome() + " | " + tbl.getCliente().getEndereco() + " | " + tbl.getValor());
-		}
+//		 Tbl cliente = h.buscaPorId(Tbl.class, 2693);
+//		 h.remove(cliente);
+//
+//		List<Tbl> resultado = (List<Tbl>) h.buscaPorId(Tbl.class, 1);
+//
+//		for (Tbl tbl1 : resultado) {
+//			System.out.println(tbl1.getNome() + " | " + tbl1.getQtdHospedes() + " | " + tbl1.getCheckin()
+//					+ tbl1.getCheckout() + " | " + tbl1.getNoites() + " | " + tbl1.getQuarto() + tbl1.getValorDiaria()
+//					+ " | " + tbl1.getValorPeriodo() + " | " + tbl1.getGastosExtras());
+//		}
 
 	}
 
